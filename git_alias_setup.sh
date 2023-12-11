f33325d4c0cedecd94fcfa46233c5fd1ef8be279
Javier Sierra <javess@gmail.com>
2023-12-11 23:24:47 +0000

    Git global aliases setup script...

---
+++ git_alias_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Setting up global aliases for git" 
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
