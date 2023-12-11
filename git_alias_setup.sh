233b65e7898b0c6fb6858953472692fff552b8aa
Javier Sierra <javess@gmail.com>
2023-12-11 23:26:26 +0000

    Adding smartlog to git setup

--- git_alias_setup.sh
+++ git_alias_setup.sh
@@ -7,3 +7,5 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.smartlog "log --graph --pretty=format:'commit: %C(bold red)%h%Creset %C(red)<%H>%Creset %C(bold magenta)%d %Creset%ndate: %C(bold yellow)%cd %Creset%C(yellow)%cr%Creset%nauthor: %C(bold blue)%an%Creset %C(blue)<%ae>%Creset%n%C(cyan)%s%n%Creset'"
+git config --global alias.sl '!git smartlog'
