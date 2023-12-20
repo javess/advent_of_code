d512fd86919158d9102ba5d94592d34764479f52
Javier Sierra <javess@gmail.com>
2023-12-20 21:25:51 +0000

    Day 9. Easy peasy

---
+++ 2023/day9/part2/puzzle2.py
@@ -0,0 +1,34 @@
+from collections import Counter
+import functools
+
+test_files = [
+    './2023/day9/inputs_simple.txt',
+    './2023/day9/inputs_full.txt'
+]
+
+
+def navigate(readings):
+    if all([x == 0 for x in readings]):
+        return 0
+    diffs = []
+    for i in range(1, len(readings)):
+        diffs.append(readings[i]-readings[i-1])
+    val = navigate(diffs)
+    newVal = readings[0] - val
+    return newVal
+
+
+for test_file in test_files:
+    print(test_file)
+
+    with open(test_file) as file:
+        readings = []
+
+        for line in file:
+            readings.append([int(x) for x in line.strip().split(' ')])
+
+        total = 0
+        for x in readings:
+            total = total + navigate(x)
+
+        print(total)
